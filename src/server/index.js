c8e4f9323802b23663aaabc87a2644834e3fbec5
Konstantin Kulebaev <ku.kulebaev@interfax.ru>
2024-04-04 12:25:54 +0400

    build: add logger "morgan"

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const cors = require('cors')
+const morgan = require('morgan')
 
 const app = express()
 const port = 4000
 
 app.use(cors())
+app.use(morgan('tiny'))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
