59787699dd217e8135ab64be265c79d982d5249c
Konstantin Kulebaev <ku.kulebaev@interfax.ru>
2024-04-04 13:10:46 +0400

    feat: add server timeout

--- src/server/index.js
+++ src/server/index.js
@@ -2,14 +2,25 @@ const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
 const app = express()
 const port = 4000
+const SERVER_TIMEOUT = 3000
 
 app.use(cors())
 app.use(morgan('tiny'))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/ingest/:token/track', (req, res) => {
+  sleep(SERVER_TIMEOUT).then(() => {
+    res.send(req.params)
+  })
 })
 
 app.listen(port, () => {
