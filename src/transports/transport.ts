e32ea6653d16ef9a9f368209613d4f848146b56f
Konstantin Kulebaev <ku.kulebaev@interfax.ru>
2024-04-09 11:40:40 +0400

    feat: catch error using sendBeacon

--- src/transports/transport.ts
+++ src/transports/transport.ts
@@ -1,5 +1,4 @@
 import unfetch from 'unfetch'
-import { getGlobalScope } from '../utils/globalScope'
 import { deleteFalsyValuesMutable } from '../utils/deleteFalsyValuesMutable'
 
 let fetch = unfetch
@@ -12,13 +11,13 @@ const headers = { 'Content-Type': 'text/plain' }
 
 export class Transport {
   static sendBeacon(url: string, payload: object) {
-    const globalScope = getGlobalScope()
-    if (!globalScope?.navigator.sendBeacon) {
-      throw new Error('SendBeaconTransport is not supported')
-    } else {
+    /* TODO: think about logger level */
+    try {
       /* TODO: think about the middleware layer */
       const data = JSON.stringify(deleteFalsyValuesMutable(payload))
       navigator.sendBeacon(url, data)
+    } catch (e) {
+      console.error(e)
     }
   }
 
