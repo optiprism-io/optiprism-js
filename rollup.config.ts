2aef12927bbad2d00d02dff90ee6d0e7992bf426
Konstantin Kulebaev <ku.kulebaev@interfax.ru>
2024-04-11 17:07:08 +0400

    build: remove snippet options

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,26 +2,6 @@ import { defineConfig, type RollupOptions } from 'rollup'
 import typescript from '@rollup/plugin-typescript'
 import resolve from '@rollup/plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
-import { exec } from 'child_process'
-
-const optiprismSnippet = () => {
-  return {
-    name: 'optiprism-snippet',
-    options: (opt: unknown) => {
-      return new Promise(resolve => {
-        // @ts-ignore
-        opt.input = 'generated/optiprismSnippet.js'
-        if (process.env.GENERATE_SNIPPET !== 'true') return resolve(opt)
-        exec('node scripts/version/createSnippet.js', err => {
-          if (err) {
-            throw err
-          }
-          resolve(opt)
-        })
-      })
-    },
-  }
-}
 
 const umd: RollupOptions = {
   input: 'src/index.ts',
@@ -52,20 +32,6 @@ const iife: RollupOptions = {
       browser: true,
     }),
     terser(),
-  ],
-}
-
-const snippet = {
-  output: {
-    name: 'optiprism',
-    file: 'lib/scripts/optiprism-snippet-min.js',
-    format: 'iife',
-  },
-  plugins: [
-    optiprismSnippet(),
-    terser(),
-    //   execute('node scripts/version/create-snippet-instructions.js'),
-    //   execute('node scripts/version/update-readme.js'),
   ],
 }
 
