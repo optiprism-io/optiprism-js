4b2f6f096b429048c158ce9327928573083d77f6
Konstantin Kulebaev <ku.kulebaev@interfax.ru>
2024-05-16 18:56:09 +0400

    feat: save userId and groups in LS

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,7 @@
 const LS_PREFIX = 'op_'
 export const ANONYMOUS_ID_KEY = LS_PREFIX + 'anonymous_id'
+export const GROUP_ID_KEY = LS_PREFIX + 'groups'
+export const USER_ID_KEY = LS_PREFIX + 'user_id'
 
 export const EVENT_NAME_PAGE = 'Page'
 export const EVENT_NAME_CLICK = 'Click'
