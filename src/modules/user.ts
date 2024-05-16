f1b3a2fbc75dd6d482247e22c053a9d59427fd1d
Konstantin Kulebaev <ku.kulebaev@interfax.ru>
2024-05-16 18:56:09 +0400

    feat: save userId and groups in LS

--- src/modules/user.ts
+++ src/modules/user.ts
@@ -1,7 +1,7 @@
 import { ApiClient } from '../api-client/apiClient'
 import { Context } from './context'
 import { IdentifyEventRequest } from '../api'
-import { USER_GROUP } from '@/constants'
+import { USER_GROUP, USER_ID_KEY } from '@/constants'
 import { LocalStorage } from '@/modules/localStorage'
 
 export class User {
@@ -12,12 +12,14 @@ export class User {
   constructor(client: ApiClient, token: string) {
     this.apiClient = client
     this.token = token
+    this.userId = LocalStorage.get(USER_ID_KEY) as string
   }
 
   async identify(id: string, properties?: IdentifyEventRequest['properties']) {
     const context = new Context()
 
     this.userId = id
+    LocalStorage.set(USER_ID_KEY, this.userId)
 
     await this.apiClient.tracking.identifyEvent(this.token, {
       context,
