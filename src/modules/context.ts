9192a06c222c056ad074a62d12c002046bd705f1
Konstantin Kulebaev <ku.kulebaev@interfax.ru>
2024-05-17 16:35:17 +0400

    feat: add UTM to track

--- src/modules/context.ts
+++ src/modules/context.ts
@@ -1,25 +1,31 @@
 import { Context as IContext } from '../api'
 import { name, version } from '../../package.json'
+import { UTM_CONTENT, UTM_MEDIUM, UTM_NAME, UTM_SOURCE, UTM_TERM } from '@/constants'
 
 export class Context implements IContext {
   library?: IContext['library']
   page?: IContext['page']
   userAgent?: IContext['userAgent']
-  ip?: IContext['ip']
+  campaign?: IContext['campaign']
 
   constructor() {
-    this.library = {
-      name,
-      version,
-    }
-    this.page = {
-      path: location.pathname,
-      referrer: document.referrer,
-      search: location.search,
-      title: document.title,
-      url: location.href,
-    }
+    const path = location.pathname
+    const referrer = document.referrer
+    const search = location.search
+    const title = document.title
+    const url = location.href
+
+    const URlSearchParams = new URL(url).searchParams
+    const source = URlSearchParams.get(UTM_SOURCE)
+    const medium = URlSearchParams.get(UTM_MEDIUM) || undefined
+    const term = URlSearchParams.get(UTM_TERM) || undefined
+    const content = URlSearchParams.get(UTM_CONTENT) || undefined
+    const campaign = URlSearchParams.get(UTM_NAME) || undefined
+
+    this.library = { name, version }
+    this.page = { path, referrer, search, title, url }
     this.userAgent = navigator.userAgent
+    if (source) this.campaign = { source, medium, term, content, name: campaign }
 
     // locale:
     //   navigator.languages && navigator.languages.length
