46d1520e124dafe8c907c55f4429f762f20e4680
Konstantin Kulebaev <ku.kulebaev@interfax.ru>
2024-05-21 10:24:12 +0400

    feat: create remove empty values middleware

--- src/api-client/middlewares.ts
+++ src/api-client/middlewares.ts
@@ -1,5 +1,6 @@
 import { ConsolaInstance } from 'consola'
 import { ErrorContext, Middleware, RequestContext } from '../api'
+import { cleanEmptyValues } from '@/utils/cleanEmptyValues'
 
 export class LoggerMiddleware implements Middleware {
   logger: ConsolaInstance
@@ -9,12 +10,24 @@ export class LoggerMiddleware implements Middleware {
   }
 
   pre(context: RequestContext) {
-    this.logger.info('Send Request:', context)
+    this.logger.debug('Send Request:', { url: context.url, body: context.init.body })
     return Promise.resolve(context)
   }
 
   onError(context: ErrorContext) {
     this.logger.error('Request Error:', context.error)
     return Promise.reject()
+  }
+}
+
+export class RemoveEmptyMiddleware implements Middleware {
+  pre(context: RequestContext) {
+    const cleanContext = { ...context }
+    if (typeof cleanContext.init.body === 'string') {
+      const jsonBody = JSON.parse(cleanContext.init.body)
+      const data = cleanEmptyValues(jsonBody)
+      cleanContext.init.body = data
+    }
+    return Promise.resolve(cleanContext)
   }
 }
