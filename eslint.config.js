e37133c2d53b05289b4568e9d0f2692a5ba562b9
Konstantin Kulebaev <ku.kulebaev@interfax.ru>
2024-05-25 08:49:54 +0400

    build: add restricted imports "..*"

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
-import globals from 'globals'
 import pluginJs from '@eslint/js'
+import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default [
@@ -9,4 +9,19 @@ export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    rules: {
+      'no-restricted-imports': [
+        'error',
+        {
+          patterns: [
+            {
+              group: ['../*'],
+              message: 'Please use pattern "./*" for relative imports instead of "../*"',
+            },
+          ],
+        },
+      ],
+    },
+  },
 ]
