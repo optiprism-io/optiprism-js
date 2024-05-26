22a941ce1245b2de0d5ff58d85343cef64e2fcee
Konstantin Kulebaev <ku.kulebaev@interfax.ru>
2024-05-26 11:11:32 +0400

    feat: add method register

--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,7 @@ const LS_PREFIX = 'op_'
 export const ANONYMOUS_ID_KEY = LS_PREFIX + 'anonymous_id'
 export const GROUP_ID_KEY = LS_PREFIX + 'groups'
 export const USER_ID_KEY = LS_PREFIX + 'user_id'
+export const SUPER_PROPERTIES_KEY = LS_PREFIX + 'super_props'
 
 export const EVENT_NAME_PAGE = 'Page'
 export const EVENT_NAME_CLICK = 'Click'
