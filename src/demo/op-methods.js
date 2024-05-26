0faf974969302da3e10f0e612aad15eb91b49501
Konstantin Kulebaev <ku.kulebaev@interfax.ru>
2024-05-26 10:46:34 +0400

    chore: add test register btn

--- src/demo/op-methods.js
+++ src/demo/op-methods.js
@@ -1,5 +1,13 @@
 const resetBtn = document.querySelectorAll('[data-reset]')[0]
+const registerBtn = document.querySelectorAll('[data-register]')[0]
+
+const TEST_SUPER_KEY = 'SuperPropKey'
+const TEST_SUPER_VALUE = 'SuperPropValue'
 
 resetBtn.addEventListener('click', () => {
   globalThis.optiprism.reset()
+})
+
+registerBtn.addEventListener('click', () => {
+  globalThis.optiprism.register({ [TEST_SUPER_KEY]: TEST_SUPER_VALUE })
 })
