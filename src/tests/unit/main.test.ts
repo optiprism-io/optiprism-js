8099604883b009a025c9f0f91ad34f5f87e017a8
Konstantin Kulebaev <ku.kulebaev@interfax.ru>
2024-05-28 22:57:03 +0400

    test: add test for register super props

--- src/tests/unit/main.test.ts
+++ src/tests/unit/main.test.ts
@@ -1,20 +1,23 @@
 import { faker } from '@faker-js/faker'
-import { beforeEach, describe, expect, it, vi } from 'vitest'
+import { MockInstance, beforeEach, describe, expect, it, vi } from 'vitest'
 
+import { ANONYMOUS_ID_KEY } from '@/constants'
 import { OptiprismBrowser } from '@/index'
-import { LocalStorage } from '@/modules/localStorage'
 import { LogLevel } from '@/utils/logLevel'
 
-describe('OptiPrism class', () => {
+describe('OptiPrism class should', () => {
   let optiprism: OptiprismBrowser
+  let apiTrackMock: MockInstance
+  const TEST_TOKEN = faker.string.uuid()
 
   beforeEach(() => {
     optiprism = new OptiprismBrowser({ loggerLevel: LogLevel.Warnings })
+    apiTrackMock = vi.spyOn(optiprism.__apiClient.tracking, 'trackEvent')
+    global.navigator.sendBeacon = vi.fn(() => true)
+    window.localStorage.clear()
   })
 
-  it('should have token in own config after initializing', () => {
-    const TEST_TOKEN = faker.string.uuid()
-
+  it('have token in own config after initializing', () => {
     optiprism.configure({
       token: TEST_TOKEN,
     })
@@ -22,7 +25,7 @@ describe('OptiPrism class', () => {
     expect(optiprism.config).toEqual(expect.objectContaining({ token: TEST_TOKEN }))
   })
 
-  it('should have log error when token is not exist', () => {
+  it('have log error when token is not exist', () => {
     optiprism.configure({
       token: '',
     })
@@ -31,9 +34,7 @@ describe('OptiPrism class', () => {
     expect(optiprism.__logger._lastLog.object?.args[0]).toEqual('Token is required')
   })
 
-  it('should call sendBeacon when track is called', async () => {
-    global.navigator.sendBeacon = vi.fn(() => true)
-
+  it('call sendBeacon when track is called', async () => {
     optiprism.configure({
       token: faker.string.uuid(),
       autotrack: false,
@@ -43,23 +44,20 @@ describe('OptiPrism class', () => {
     expect(navigator.sendBeacon).toHaveBeenCalledTimes(1)
   })
 
-  it('should have anonymousId from local storage', () => {
-    const TEST_ANONYMOUS_ID = faker.string.uuid()
-    vi.spyOn(LocalStorage, 'get').mockReturnValue(TEST_ANONYMOUS_ID)
-
+  it('have anonymousId from local storage', () => {
     optiprism.configure({
       token: faker.string.uuid(),
       autotrack: false,
     })
 
-    expect(optiprism.config.anonymousId).toEqual(TEST_ANONYMOUS_ID)
+    expect(JSON.stringify(optiprism.config.anonymousId)).toEqual(
+      window.localStorage.getItem(ANONYMOUS_ID_KEY)
+    )
   })
 
-  it('should call identify method with correct args when method is called for user', async () => {
-    global.navigator.sendBeacon = vi.fn(() => true)
+  it('call identify method with correct args when method is called for user', async () => {
     const identifyMock = vi.spyOn(optiprism.__apiClient.tracking, 'identifyEvent')
     const TEST_FULL_NAME = faker.person.fullName()
-    const TEST_TOKEN = faker.string.uuid()
 
     optiprism.configure({
       token: TEST_TOKEN,
@@ -68,17 +66,14 @@ describe('OptiPrism class', () => {
     await optiprism.user?.identify(TEST_FULL_NAME)
 
     expect(identifyMock).toHaveBeenCalledTimes(1)
-    expect(identifyMock.mock.calls[0][0]).toEqual(TEST_TOKEN)
     expect(identifyMock.mock.calls[0][1]?.group).toEqual('user')
     expect(identifyMock.mock.calls[0][1]?.id).toEqual(TEST_FULL_NAME)
   })
 
-  it('should call identify method with correct args when method is called for group', async () => {
-    global.navigator.sendBeacon = vi.fn(() => true)
-    const identifyMock = vi.spyOn(optiprism.__apiClient.tracking, 'identifyEvent')
+  it('call identify method with correct args when method is called for group', async () => {
+    const apiIdentifyMockInstance = vi.spyOn(optiprism.__apiClient.tracking, 'identifyEvent')
     const TEST_COMPANY_NAME = faker.company.name()
     const TEST_FULL_NAME = faker.person.fullName()
-    const TEST_TOKEN = faker.string.uuid()
 
     optiprism.configure({
       token: TEST_TOKEN,
@@ -86,17 +81,13 @@ describe('OptiPrism class', () => {
     })
     await optiprism.group?.identify(TEST_COMPANY_NAME, TEST_FULL_NAME)
 
-    expect(identifyMock).toHaveBeenCalledTimes(1)
-    expect(identifyMock.mock.calls[0][0]).toEqual(TEST_TOKEN)
-    expect(identifyMock.mock.calls[0][1]?.group).toEqual(TEST_COMPANY_NAME)
-    expect(identifyMock.mock.calls[0][1]?.id).toEqual(TEST_FULL_NAME)
+    expect(apiIdentifyMockInstance).toHaveBeenCalledTimes(1)
+    expect(apiIdentifyMockInstance.mock.calls[0][1]?.group).toEqual(TEST_COMPANY_NAME)
+    expect(apiIdentifyMockInstance.mock.calls[0][1]?.id).toEqual(TEST_FULL_NAME)
   })
 
-  it('should call track with correct args when user is identified', async () => {
-    global.navigator.sendBeacon = vi.fn(() => true)
-    const identifyMock = vi.spyOn(optiprism.__apiClient.tracking, 'trackEvent')
+  it('call track with correct args when user is identified', async () => {
     const TEST_FULL_NAME = faker.person.fullName()
-    const TEST_TOKEN = faker.string.uuid()
 
     optiprism.configure({
       token: TEST_TOKEN,
@@ -105,18 +96,14 @@ describe('OptiPrism class', () => {
     await optiprism.user?.identify(TEST_FULL_NAME)
     await optiprism.track('click')
 
-    expect(identifyMock.mock.calls[0][0]).toEqual(TEST_TOKEN)
-    expect(identifyMock.mock.calls[0][1]?.userId).toEqual(TEST_FULL_NAME)
+    expect(apiTrackMock.mock.calls[0][1]?.userId).toEqual(TEST_FULL_NAME)
   })
 
-  it('should call track with correct args when groups are identified', async () => {
-    global.navigator.sendBeacon = vi.fn(() => true)
-    const identifyMock = vi.spyOn(optiprism.__apiClient.tracking, 'trackEvent')
+  it('call track with correct args when groups are identified', async () => {
     const TEST_COMPANY_NAME_1 = faker.company.name()
     const TEST_COMPANY_NAME_2 = faker.company.name()
     const TEST_FULL_NAME_1 = faker.person.fullName()
     const TEST_FULL_NAME_2 = faker.person.fullName()
-    const TEST_TOKEN = faker.string.uuid()
 
     optiprism.configure({
       token: TEST_TOKEN,
@@ -126,10 +113,105 @@ describe('OptiPrism class', () => {
     await optiprism.group?.identify(TEST_COMPANY_NAME_2, TEST_FULL_NAME_2)
     await optiprism.track('click')
 
-    expect(identifyMock.mock.calls[0][0]).toEqual(TEST_TOKEN)
-    expect(identifyMock.mock.calls[0][1]?.groups).toEqual({
+    expect(apiTrackMock.mock.calls[0][1]?.groups).toEqual({
       [TEST_COMPANY_NAME_1]: TEST_FULL_NAME_1,
       [TEST_COMPANY_NAME_2]: TEST_FULL_NAME_2,
     })
+  })
+
+  it('pass super props to track when called register method', async () => {
+    const TEST_SUPER_PROPS_KEY = 'location'
+    const TEST_SUPER_PROP_VALUE = faker.location.country()
+    const TEST_SUPER_PROPS = { [TEST_SUPER_PROPS_KEY]: TEST_SUPER_PROP_VALUE }
+
+    optiprism.configure({
+      token: TEST_TOKEN,
+      autotrack: false,
+    })
+    optiprism.register(TEST_SUPER_PROPS)
+    await optiprism.track('click')
+
+    expect(apiTrackMock.mock.calls[0][1]?.properties).toEqual(TEST_SUPER_PROPS)
+  })
+
+  it('pass super props to track when called register_once method', async () => {
+    const TEST_SUPER_PROPS_KEY = 'location'
+    const TEST_SUPER_PROP_VALUE = faker.location.country()
+    const TEST_SUPER_PROPS = { [TEST_SUPER_PROPS_KEY]: TEST_SUPER_PROP_VALUE }
+
+    optiprism.configure({
+      token: TEST_TOKEN,
+      autotrack: false,
+    })
+    optiprism.register_once(TEST_SUPER_PROPS)
+    await optiprism.track('click')
+
+    expect(apiTrackMock.mock.calls[0][1]?.properties).toEqual(TEST_SUPER_PROPS)
+  })
+
+  it('NOT overwrite super props when called register_once method again', async () => {
+    const TEST_SUPER_PROPS_KEY = 'location'
+    const TEST_SUPER_PROP_VALUE = faker.location.country()
+    const TEST_SUPER_PROP_VALUE_2 = faker.location.city()
+    const TEST_SUPER_PROPS = { [TEST_SUPER_PROPS_KEY]: TEST_SUPER_PROP_VALUE }
+    const TEST_SUPER_PROPS_2 = { [TEST_SUPER_PROPS_KEY]: TEST_SUPER_PROP_VALUE_2 }
+
+    optiprism.configure({
+      token: TEST_TOKEN,
+      autotrack: false,
+    })
+    optiprism.register_once(TEST_SUPER_PROPS)
+    optiprism.register_once(TEST_SUPER_PROPS_2)
+    await optiprism.track('click')
+
+    expect(apiTrackMock.mock.calls[0][1]?.properties).toEqual(TEST_SUPER_PROPS)
+    expect(apiTrackMock.mock.calls[0][1]?.properties).not.toEqual(TEST_SUPER_PROPS_2)
+  })
+
+  it('overwrite super props when called register method again', async () => {
+    const TEST_SUPER_PROPS_KEY = 'location'
+    const TEST_SUPER_PROP_VALUE = faker.location.country()
+    const TEST_SUPER_PROP_VALUE_2 = faker.location.city()
+    const TEST_SUPER_PROPS = { [TEST_SUPER_PROPS_KEY]: TEST_SUPER_PROP_VALUE }
+    const TEST_SUPER_PROPS_2 = { [TEST_SUPER_PROPS_KEY]: TEST_SUPER_PROP_VALUE_2 }
+
+    optiprism.configure({
+      token: TEST_TOKEN,
+      autotrack: false,
+    })
+    optiprism.register(TEST_SUPER_PROPS)
+    optiprism.register(TEST_SUPER_PROPS_2)
+    await optiprism.track('click')
+
+    expect(apiTrackMock.mock.calls[0][1]?.properties).toEqual(TEST_SUPER_PROPS_2)
+    expect(apiTrackMock.mock.calls[0][1]?.properties).not.toEqual(TEST_SUPER_PROPS)
+  })
+
+  it('remove super props when called unregister method', async () => {
+    const TEST_SUPER_PROPS_KEY = 'location'
+    const TEST_SUPER_PROPS_KEY_2 = 'location_2'
+    const TEST_SUPER_PROP_VALUE = faker.location.country()
+    const TEST_SUPER_PROP_VALUE_2 = faker.location.city()
+    const TEST_SUPER_PROPS = { [TEST_SUPER_PROPS_KEY]: TEST_SUPER_PROP_VALUE }
+    const TEST_SUPER_PROPS_2 = { [TEST_SUPER_PROPS_KEY_2]: TEST_SUPER_PROP_VALUE_2 }
+
+    optiprism.configure({
+      token: TEST_TOKEN,
+      autotrack: false,
+    })
+    optiprism.register(TEST_SUPER_PROPS)
+    optiprism.register_once(TEST_SUPER_PROPS_2)
+    optiprism.unregister(TEST_SUPER_PROPS_KEY)
+    optiprism.unregister(TEST_SUPER_PROPS_KEY_2)
+    await optiprism.track('click')
+
+    expect(apiTrackMock.mock.calls[0][1]?.properties[TEST_SUPER_PROPS_KEY]).toBeUndefined()
+    expect(apiTrackMock.mock.calls[0][1]?.properties[TEST_SUPER_PROPS_KEY_2]).toBeUndefined()
+    expect(apiTrackMock.mock.calls[0][1]?.properties[TEST_SUPER_PROPS_KEY]).not.toEqual(
+      TEST_SUPER_PROP_VALUE
+    )
+    expect(apiTrackMock.mock.calls[0][1]?.properties[TEST_SUPER_PROPS_KEY]).not.toEqual(
+      TEST_SUPER_PROP_VALUE_2
+    )
   })
 })
