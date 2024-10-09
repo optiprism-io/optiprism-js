9d724693f2fc9127182305ff584dc687ef40ad4d
kkulebaev <konstantinkulebaev@gmail.com>
2024-10-09 20:55:49 +0300

    feat: add server url to configure tracker

--- src/modules/config.ts
+++ src/modules/config.ts
@@ -6,17 +6,20 @@ interface Autotrack {
 
 export interface Config {
   token: string
+  serverUrl: string
   autotrack?: Autotrack | boolean
   anonymousId?: string
 }
 
 export class OptiConfig {
   token: Config['token']
+  serverUrl: Config['serverUrl']
   autotrack?: Config['autotrack']
   anonymousId?: Config['anonymousId']
 
   constructor(config?: Config) {
     this.token = config?.token || ''
+    this.serverUrl = config?.serverUrl || ''
     this.autotrack = config?.autotrack || true
     this.anonymousId = config?.anonymousId
   }
